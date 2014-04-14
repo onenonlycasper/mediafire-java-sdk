860616f7e0262536ddf640a055dfd62950438db0
John Moore <johnmoore@mediafire.com>
2014-04-14 12:32:04 -0500

    Replaced StringBuilder with String

--- src/main/java/com/arkhive/components/api/filesystem/FileSystemItem.java
+++ src/main/java/com/arkhive/components/api/filesystem/FileSystemItem.java
@@ -1,7 +1,7 @@
 package com.arkhive.components.api.filesystem;
 
 /**
- * 
+ *
  * @author john moore
  *
  */
@@ -21,7 +21,7 @@ public class FileSystemItem {
     private int revisionEpoch;
     private long revision;
 
-    
+
   public String getThumbnailBase64Encoded() {
     if (thumbnail == null) { thumbnail = ""; }
     return thumbnail;
@@ -30,29 +30,29 @@ public class FileSystemItem {
    * The hash of the item. only applies to files.
    * @return the hash of the file.
    */
-  public String getHash() { 
+  public String getHash() {
     if (hash == null) { hash = ""; }
-    return hash; 
+    return hash;
   }
-  
+
   /** Indicated if the item a folder.
-   * @return true if the item is a folder. 
+   * @return true if the item is a folder.
    */
   public boolean isFolder() { return isFolder; }
 
   /** The key of the items parent.
-   * @return the key of the items parent. 
+   * @return the key of the items parent.
    */
-  public String getParentFolderKey() { 
+  public String getParentFolderKey() {
     if (parentFolderKey == null) { parentFolderKey = ""; }
-    return parentFolderKey; 
+    return parentFolderKey;
   }
 
   /** The key for the item.
    * For a folder this is the folder key, for a file it is the quickey.
-   * @return The key of the item. 
+   * @return The key of the item.
    */
-  public String getKey() { 
+  public String getKey() {
     if (key == null) { key = ""; }
     return key;
   }
@@ -60,49 +60,49 @@ public class FileSystemItem {
   /** The name of the item.
    * @return The name of the item.
    */
-  public String getName() { 
+  public String getName() {
     if (name == null) { name = ""; }
-    return name; 
+    return name;
   }
 
   /** The mimetype of the item.
    * @return the mimeType
    */
-  public String getMimeType() { 
+  public String getMimeType() {
     if (mimeType == null) { mimeType = ""; }
-    return mimeType; 
+    return mimeType;
   }
 
   /** The description of the item.
    * @return The description of the item.
    */
-  public String getDescription() { 
+  public String getDescription() {
     if (description == null) { description = ""; }
-    return description; 
+    return description;
   }
 
   /** The date the item was created in the cloud.
    * @return The items creation date.
    */
-  public String getCreatedDate() { 
+  public String getCreatedDate() {
     if (createdDate == null) { createdDate = ""; }
-    return createdDate; 
-  } 
+    return createdDate;
+  }
 
   /** Indicates if the file is shared with other users.
    * @return The sharing status of the item.
    */
-  public boolean isShared() { return isShared; } 
+  public boolean isShared() { return isShared; }
 
   /** Indicates if the file is shared from another user.
    * @return The sharing status of the item.
    */
-  public boolean isSharedFromOther() { return isSharedFromOther; } 
+  public boolean isSharedFromOther() { return isSharedFromOther; }
 
   /** The items size in bytes.
    * @return The size of the item.
    */
-  public long getSize() { return size; } 
+  public long getSize() { return size; }
 
   /** The epoch of the last revision.
    * @return The epoch time of the last revision.
@@ -112,32 +112,30 @@ public class FileSystemItem {
   /** The revision number of the last revision.
    * @return The revision number.
    */
-  public long getRevision() { return revision; } 
+  public long getRevision() { return revision; }
 
   /**
-   * 
+   *
    */
   @Override
   public String toString() {
-    StringBuilder strb = new StringBuilder();
-    strb.append("key: ").append(getKey()).append(", ");
-    strb.append("parentFolderKey: ").append(getParentFolderKey()).append(", ");
-    strb.append("isFolder: ").append(isFolder()).append(", ");
-    strb.append("name: ").append(getName()).append(", ");
-    strb.append("mimeType: ").append(getMimeType()).append(", ");
-    strb.append("description: ").append(getDescription()).append(", ");
-    strb.append("createdDate: ").append(getCreatedDate()).append(", ");
-    strb.append("isShared: ").append(isShared()).append(", ");
-    strb.append("isSharedFromOther: ").append(isSharedFromOther()).append(", ");
-    strb.append("size: ").append(getSize()).append(", ");
-    strb.append("hash: ").append(getHash()).append(", ");
-    strb.append("revisionEpoch: ").append(getRevisionEpoch()).append(", ");
-    strb.append("revision: ").append(getRevision());
-    
-    return strb.toString();
+
+      return "key: " + getKey() + ", "
+              + "parentFolderKey: " + getParentFolderKey() + ", "
+              + "isFolder: " + isFolder() + ", "
+              + "name: " + getName() + ", "
+              + "mimeType: " + getMimeType() + ", "
+              + "description: " + getDescription() + ", "
+              + "createdDate: " + getCreatedDate() + ", "
+              + "isShared: " + isShared() + ", "
+              + "isSharedFromOther: " + isSharedFromOther() + ", "
+              + "size: " + getSize() + ", "
+              + "hash: " + getHash() + ", "
+              + "revisionEpoch: " + getRevisionEpoch() + ", "
+              + "revision: " + getRevision();
   }
   /**
-   * 
+   *
    * @author john moore
    *
    */
@@ -209,7 +207,7 @@ public class FileSystemItem {
             item.revisionEpoch = value;
             return this;
         }
-        
+
         public Builder hash(String value) {
           if (value == null) {
             value = "";
@@ -217,7 +215,7 @@ public class FileSystemItem {
           item.hash = value;
           return this;
         }
-        
+
         public Builder thumbnailBase64Encoded(String value) {
           if (value == null) {
             value = "";
