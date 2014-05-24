03db4607c7b8763a058806a870a0cf0e9afb6a51
Christopher Najar <chris.najar@mediafire.com>
2014-05-24 14:38:15 -0500

    removed net checker.

--- src/main/java/com/arkhive/components/uploadmanager/manager/NetChecker.java
+++
@@ -1,9 +0,0 @@
-package com.arkhive.components.uploadmanager.manager;
-
-/**
- * Created by Chris Najar on 5/23/2014.
- */
-public interface NetChecker {
-    public void lostConnection();
-    public void acquiredConnection();
-}
