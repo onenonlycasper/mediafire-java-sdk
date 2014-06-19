310b47847861d6ce5e408259d151171ae3aecb63
Christopher Najar <chris.najar@mediafire.com>
2014-06-19 10:41:30 -0500

    added expiration field to action token.

--- src/main/java/com/arkhive/components/test_session_manager_fixes/module_token_farm/tokens/ActionToken.java
+++ src/main/java/com/arkhive/components/test_session_manager_fixes/module_token_farm/tokens/ActionToken.java
@@ -5,6 +5,7 @@ package com.arkhive.components.test_session_manager_fixes.module_token_farm.toke
  */
 public final class ActionToken extends Token {
     private final Type type;
+    private long expiration;
 
     private ActionToken(Type type) {
         super();
@@ -23,7 +24,35 @@ public final class ActionToken extends Token {
         return type;
     }
 
+    /**
+     * Sets the expiration of the token. The time should use System.currentTimeMillis() to get the current time
+     * and then add 24 hours since all action tokens are set to 24 hour expiration time.
+     * @param expiration
+     */
+    public void setExpiration(long expiration) {
+        this.expiration = expiration;
+    }
+
+    /**
+     * All action tokens are set to 24 hour expiration time, but if the expiration time is in 1 hour, then it is counted
+     * as expired.
+     * @return false if not within 1 hour of expiration, true if at most within 1 hour of expiration.
+     */
+    public boolean isExpired() {
+        return (System.currentTimeMillis() + 3600000) >= expiration;
+    }
+
     public enum Type {
-        UPLOAD, IMAGE,
+        UPLOAD("upload"),
+        IMAGE("image");
+
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+
+        public String toString() {
+            return value;
+        }
     }
 }
