6c1b57adc5046efd35a9825bc178e30330d6e71c
Christopher Najar <chris.najar@mediafire.com>
2014-06-19 10:39:38 -0500

    added driver to test action token functionality

--- src/test/java/DriverActionTokenFunctionalityDoNotDelete.java
+++ src/test/java/DriverActionTokenFunctionalityDoNotDelete.java
@@ -1,8 +1,38 @@
+import com.arkhive.components.test_session_manager_fixes.Configuration;
+import com.arkhive.components.test_session_manager_fixes.MediaFire;
+import com.arkhive.components.test_session_manager_fixes.module_api.responses.ApiResponse;
+import com.arkhive.components.test_session_manager_fixes.module_api_descriptor.interfaces.ApiRequestRunnableCallback;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by Chris Najar on 6/18/2014.
  */
 public class DriverActionTokenFunctionalityDoNotDelete {
-    public static void main(String[] args) {
+    static volatile int errors = 0;
+    static volatile int signatureErrors = 0;
+    static volatile int sessionTokenErrors = 0;
 
+    private static final String TAG = DriverSessionTokenFunctionalityDoNotDelete.class.getSimpleName();
+    public static void main(String[] args) {
+        String apiKey = "1ngvq4h5rn8om4at7u9884z9i3sbww44b923w5ee";
+        String appId = "35";
+        Configuration configuration = new Configuration();
+        configuration.setApiKey(apiKey);
+        configuration.setAppId(appId);
+        configuration.setHttpConnectionTimeout(5000);
+        configuration.setHttpReadTimeout(5000);
+        configuration.setSessionTokensInBlockingQueueMinMax(1, 6);
+
+        MediaFire mediaFire = MediaFire.newInstance(configuration);
+
+        Map<String, String> credentials = new HashMap<String, String>();
+        credentials.put("email", "arkhivetest@test.com");
+        credentials.put("password", "74107410");
+        mediaFire.getApplicationCredentials().setUserCredentials(credentials);
+        mediaFire.getApplicationCredentials().setCredentialsValid(true);
+        mediaFire.startup();
     }
 }
