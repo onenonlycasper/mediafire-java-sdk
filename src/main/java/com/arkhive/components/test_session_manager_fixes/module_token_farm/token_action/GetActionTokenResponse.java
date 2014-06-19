e49330e7d08a173a9f962f3c930fc928e679c4a3
Christopher Najar <chris.najar@mediafire.com>
2014-06-19 10:40:24 -0500

    action token response class.

--- src/main/java/com/arkhive/components/test_session_manager_fixes/module_token_farm/token_action/GetActionTokenResponse.java
+++ src/main/java/com/arkhive/components/test_session_manager_fixes/module_token_farm/token_action/GetActionTokenResponse.java
@@ -1,7 +1,14 @@
 package com.arkhive.components.test_session_manager_fixes.module_token_farm.token_action;
 
+import com.arkhive.components.test_session_manager_fixes.module_api.responses.ApiResponse;
+
 /**
- * Created by  on 6/17/2014.
+ * Created by Chris Najar on 6/18/2014.
  */
-public class GetActionTokenResponse {
+public class GetActionTokenResponse extends ApiResponse {
+    public String action_token;
+
+    public String getActionToken() {
+        return action_token;
+    }
 }
