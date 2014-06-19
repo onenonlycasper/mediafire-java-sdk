bdc03fb752fd8ad7b3b3186217dbe9487ebe2cb6
Christopher Najar <chris.najar@mediafire.com>
2014-06-19 15:06:30 -0500

    testing base

--- src/test/java/DriverActionTokenFunctionalityDoNotDelete.java
+++ src/test/java/DriverActionTokenFunctionalityDoNotDelete.java
@@ -1,7 +1,5 @@
 import com.arkhive.components.test_session_manager_fixes.Configuration;
 import com.arkhive.components.test_session_manager_fixes.MediaFire;
-import com.arkhive.components.test_session_manager_fixes.module_api.responses.ApiResponse;
-import com.arkhive.components.test_session_manager_fixes.module_api_descriptor.interfaces.ApiRequestRunnableCallback;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -11,11 +9,11 @@ import java.util.Map;
  * Created by Chris Najar on 6/18/2014.
  */
 public class DriverActionTokenFunctionalityDoNotDelete {
+    private static final String TAG = DriverActionTokenFunctionalityDoNotDelete.class.getSimpleName();
     static volatile int errors = 0;
     static volatile int signatureErrors = 0;
     static volatile int sessionTokenErrors = 0;
 
-    private static final String TAG = DriverSessionTokenFunctionalityDoNotDelete.class.getSimpleName();
     public static void main(String[] args) {
         String apiKey = "1ngvq4h5rn8om4at7u9884z9i3sbww44b923w5ee";
         String appId = "35";
