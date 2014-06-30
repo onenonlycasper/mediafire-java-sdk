3f1dc5f8481e05a671eec9cee1198fb1de46c1c2
Christopher Najar <chris.najar@mediafire.com>
2014-06-30 10:51:07 -0500

    test use of sendHttpsGetRequest() method.

--- src/main/java/com/arkhive/components/core/module_token_farm/runnables/GetSessionTokenRunnable.java
+++ src/main/java/com/arkhive/components/core/module_token_farm/runnables/GetSessionTokenRunnable.java
@@ -58,7 +58,7 @@ public class GetSessionTokenRunnable implements Runnable, HttpRequestCallback {
             // create request object
             apiRequestObject = createApiRequestObjectForNewSessionToken();
             // send request to http handler
-            httpPeriProcessor.sendGetRequest(this, httpPreProcessor, httpPostProcessor, apiRequestObject);
+            httpPeriProcessor.sendHttpsGetRequest(this, httpPreProcessor, httpPostProcessor, apiRequestObject);
             try {
                 wait(Configuration.DEFAULT_HTTP_CONNECTION_TIMEOUT);
             } catch (InterruptedException e) {
