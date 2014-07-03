2367c5217479f73d434f82a53fbbb361f8e8765e
Christopher Najar <chris.najar@mediafire.com>
2014-07-03 11:24:45 -0500

    added ekey field to GetSessionTokenResponse.

--- src/main/java/com/arkhive/components/core/module_api/responses/GetSessionTokenResponse.java
+++ src/main/java/com/arkhive/components/core/module_api/responses/GetSessionTokenResponse.java
@@ -7,6 +7,7 @@ public class GetSessionTokenResponse extends ApiResponse {
     private String session_token;
     private String secret_key;
     private String pkey;
+    private String ekey;
 
     public String getSessionToken() {
         return session_token;
@@ -18,5 +19,9 @@ public class GetSessionTokenResponse extends ApiResponse {
 
     public String getPkey() {
         return pkey;
+    }
+
+    public String getEkey() {
+        return ekey;
     }
 }
