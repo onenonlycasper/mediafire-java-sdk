276a8cc77a08becb24b7bf22d35aeba877d9026c
Christopher Najar <chris.najar@mediafire.com>
2014-07-08 16:30:12 -0500

    package moved. added Pausable interface.

---
+++ src/main/java/com/arkhive/components/uploadmanager/interfaces/Pausable.java
@@ -0,0 +1,12 @@
+package com.arkhive.components.uploadmanager.interfaces;
+
+/**
+ * Created by Chris Najar on 7/8/2014.
+ */
+public interface Pausable {
+    public void pause();
+
+    public void resume();
+
+    public boolean isPaused();
+}
