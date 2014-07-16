4bce7629567180977b6349d6b8c42b225dfa0e7f
Christopher Najar <chris.najar@mediafire.com>
2014-07-16 15:47:52 -0500

    added MFHost enum to store host names.

---
+++ src/main/java/com/arkhive/components/core/module_api/MFHost.java
@@ -0,0 +1,39 @@
+package com.arkhive.components.core.module_api;
+
+public enum MFHost {
+    LIVE_HTTP("www.mediafire.com", TransferScheme.HTTP),
+    LIVE_HTTPS("www.mediafire.com", TransferScheme.HTTPS),
+    DEV_HTTP("dev.mediafire.com", TransferScheme.HTTP),
+    DEV_HTTPS("dev.mediafire.com", TransferScheme.HTTPS);
+
+    private String host;
+    private TransferScheme transferScheme;
+
+    private MFHost(String host, TransferScheme transferScheme) {
+        this.host = host;
+        this.transferScheme = transferScheme;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public TransferScheme getTransferScheme() {
+        return transferScheme;
+    }
+
+    public enum TransferScheme {
+        HTTP("http://"),
+        HTTPS("https://");
+
+        private String scheme;
+
+        private TransferScheme(String scheme) {
+            this.scheme = scheme;
+        }
+
+        public String getScheme() {
+            return scheme;
+        }
+    }
+}
