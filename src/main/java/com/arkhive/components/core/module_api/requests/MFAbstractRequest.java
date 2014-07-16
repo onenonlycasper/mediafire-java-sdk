333eec4dbc6e41a94f3db63b7187617387d9828a
Christopher Najar <chris.najar@mediafire.com>
2014-07-16 16:10:42 -0500

    MFAbstractRequest. requires Map<String, String> as parameter.

--- src/main/java/com/arkhive/components/core/module_api/requests/MFAbstractRequest.java
+++ src/main/java/com/arkhive/components/core/module_api/requests/MFAbstractRequest.java
@@ -1,28 +1,42 @@
 package com.arkhive.components.core.module_api.requests;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by Chris Najar on 7/16/2014.
  */
 public abstract class MFAbstractRequest {
     private final MFHost mfHost;
     private final MFApi mfApi;
+    private final Map<String, String> requestParameters;
 
-    protected MFAbstractRequest(MFHost mfHost, MFApi mfApi) {
+    public MFAbstractRequest(MFHost mfHost, MFApi mfApi, Map<String, String> requestParameters) {
         if (mfHost == null) {
             throw new IllegalArgumentException("MFHost cannot be null");
         }
         if (mfApi == null) {
             throw new IllegalArgumentException("MFApi cannot be null");
         }
+        if (requestParameters == null) {
+            requestParameters = new LinkedHashMap<String, String>();
+            requestParameters.put("response_format", "json");
+        }
         this.mfHost = mfHost;
         this.mfApi = mfApi;
+        this.requestParameters = requestParameters;
+
     }
 
-    protected MFHost getMfHost() {
+    public MFHost getMfHost() {
         return mfHost;
     }
 
-    protected MFApi getMfApi() {
+    public MFApi getMfApi() {
         return mfApi;
+    }
+
+    public Map<String, String> getRequestParameters() {
+        return requestParameters;
     }
 }
