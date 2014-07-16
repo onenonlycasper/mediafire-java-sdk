8f3a30f0d3870a45094496b6c8439db7cb4365f5
Christopher Najar <chris.najar@mediafire.com>
2014-07-15 20:24:04 -0500

    removed runnable (not used).

--- src/main/java/com/arkhive/components/core/module_api/System.java
+++ src/main/java/com/arkhive/components/core/module_api/System.java
@@ -2,9 +2,7 @@ package com.arkhive.components.core.module_api;
 
 import com.arkhive.components.core.module_api.responses.SystemGetInfoResponse;
 import com.arkhive.components.core.module_api_descriptor.ApiRequestObject;
-import com.arkhive.components.core.module_api_descriptor.interfaces.ApiRequestRunnableCallback;
 import com.arkhive.components.core.module_api_descriptor.requests.BlockingApiGetRequest;
-import com.arkhive.components.core.module_api_descriptor.requests.RunnableApiGetRequest;
 import com.google.gson.Gson;
 
 import java.util.Map;
@@ -13,16 +11,6 @@ import java.util.Map;
  * Created by  on 6/18/2014.
  */
 public class System {
-    public Runnable getInfo(
-            ApiRequestRunnableCallback callback,Map<String,
-            String> requiredParameters, Map<String, String> optionalParameters) {
-        ApiRequestObject apiRequestObject = new ApiRequestObject(ApiUris.LIVE_HTTP, ApiUris.URI_SYSTEM_GET_INFO);
-        apiRequestObject.setOptionalParameters(optionalParameters);
-        apiRequestObject.setRequiredParameters(requiredParameters);
-        RunnableApiGetRequest runnableApiGetRequest = Api.createApiGetRequestRunnable(SystemGetInfoResponse.class, callback, apiRequestObject);
-        return runnableApiGetRequest;
-    }
-
     public SystemGetInfoResponse getInfo(
             Map<String, String> requiredParameters,
             Map<String, String> optionalParameters) {
