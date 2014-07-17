8c8454664da0ff7c8d28dbf6c5dea0667941325d
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 02:02:27 -0500

    MFLogger - changed interface method signature.

--- src/main/java/com/mediafire/sdk/MFLogger.java
+++ src/main/java/com/mediafire/sdk/MFLogger.java
@@ -1,13 +1,11 @@
 package com.mediafire.sdk;
 
-import com.arkhive.components.core.module_api_descriptor.ApiRequestObject;
-
 /**
  * Created by  on 7/2/2014.
  */
 public interface MFLogger {
-    public void logMessage(String src, String msg);
-    public void logException(String src, Exception e);
-    public void logApiError(String src, ApiRequestObject object);
+    public void logMessage(String src, String message);
+    public void logException(String src, Exception exception);
+    public void logApiError(String src, MFHttpRequest mfHttpRequest, MFHttpResponse mfHttpResponse);
 
 }
