b34f35ce6ef87581f508c3a453a038c1c9fc7869
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 02:10:31 -0500

    MFHttpRunnable added.

---
+++ src/main/java/com/mediafire/sdk/MFHttpRunnable.java
@@ -0,0 +1,11 @@
+package com.mediafire.sdk;
+
+/**
+ * Created by Chris Najar on 7/17/2014.
+ */
+public class MFHttpRunnable implements Runnable {
+    @Override
+    public void run() {
+        
+    }
+}
