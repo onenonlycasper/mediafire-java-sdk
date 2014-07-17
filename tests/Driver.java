bd101e3c3bbaf7aa7cde3a943fc0d9fb23f7f48f
Christopher Najar <chris.najar@mediafire.com>
2014-07-16 22:54:20 -0500

    Test class for subfunctionality.

---
+++ tests/Driver.java
@@ -0,0 +1,55 @@
+import com.mediafire.sdk.http.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by Chris Najar on 7/16/2014.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        driver.systemGetInfo();
+        driver.userGetSessionToken();
+    }
+
+    public void systemGetInfo() {
+        System.out.println("\n\nSYSTEM/GET_INFO");
+        HttpClient httpClient = new HttpClient(null);
+        MFRequest request = new MFRequest(MFHost.LIVE_HTTP, MFApi.URI_SYSTEM_GET_INFO);
+        MFResponse response = httpClient.sendRequest(request);
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers ");
+        for (String key : response.getHeaders().keySet()) {
+            System.out.println(key + " - " + response.getHeaders().get(key).toString());
+        }
+        System.out.println("body - " + response.getResponseString());
+        System.out.println("body bytes");
+        for (byte b : response.getBodyBytes()) {
+            System.out.print(b);
+        }
+    }
+
+    public void userGetSessionToken() {
+        System.out.println("\n\nUSER/GET_SESSION_TOKEN");
+        HttpClient httpClient = new HttpClient(null);
+        Map<String, String> requestParameters = new LinkedHashMap<String, String>();
+        requestParameters.put("email", "javasdktest@example.com");
+        requestParameters.put("password", "74107410");
+        requestParameters.put("application_id", "35");
+        requestParameters.put("signature", "30abbbd4a3f8827d1a6408f1f2ee20d5edcc4799");
+        requestParameters.put("token_version", "2");
+        MFRequest request = new MFRequest(MFHost.LIVE_HTTP, MFApi.URI_USER_GET_SESSION_TOKEN, requestParameters);
+        MFResponse response = httpClient.sendRequest(request);
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers ");
+        for (String key : response.getHeaders().keySet()) {
+            System.out.println(key + " - " + response.getHeaders().get(key).toString());
+        }
+        System.out.println("body - " + response.getResponseString());
+        System.out.println("body bytes");
+        for (byte b : response.getBodyBytes()) {
+            System.out.print(b);
+        }
+    }
+}
