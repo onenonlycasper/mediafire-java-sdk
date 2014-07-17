ecf2d471668d5c9588c2288b98c30f1984dc0f15
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 00:41:49 -0500

    Driver - name change implemented

--- tests/Driver.java
+++ tests/Driver.java
@@ -16,8 +16,8 @@ public class Driver {
     public void systemGetInfo() {
         System.out.println("\n\nSYSTEM/GET_INFO");
         MFHttpClient mfHttpClient = new MFHttpClient(null);
-        MFRequest request = new MFRequest(MFHost.LIVE_HTTP, MFApi.URI_SYSTEM_GET_INFO);
-        MFResponse response = mfHttpClient.sendRequest(request);
+        MFHttpRequest request = new MFHttpRequest(MFHost.LIVE_HTTP, MFApi.URI_SYSTEM_GET_INFO);
+        MFHttpResponse response = mfHttpClient.sendRequest(request);
         System.out.println("status: " + response.getStatus());
         System.out.println("headers ");
         for (String key : response.getHeaders().keySet()) {
@@ -39,8 +39,8 @@ public class Driver {
         requestParameters.put("application_id", "35");
         requestParameters.put("signature", "30abbbd4a3f8827d1a6408f1f2ee20d5edcc4799");
         requestParameters.put("token_version", "2");
-        MFRequest request = new MFRequest(MFHost.LIVE_HTTP, MFApi.URI_USER_GET_SESSION_TOKEN, requestParameters);
-        MFResponse response = mfHttpClient.sendRequest(request);
+        MFHttpRequest request = new MFHttpRequest(MFHost.LIVE_HTTP, MFApi.URI_USER_GET_SESSION_TOKEN, requestParameters);
+        MFHttpResponse response = mfHttpClient.sendRequest(request);
         System.out.println("status: " + response.getStatus());
         System.out.println("headers ");
         for (String key : response.getHeaders().keySet()) {
