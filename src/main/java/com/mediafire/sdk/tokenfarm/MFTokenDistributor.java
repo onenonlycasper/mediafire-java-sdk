e437da82c2eaa20168fcecb56cafdf5aba606385
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 00:04:53 -0500

    MFTokenDistributor interface created.

---
+++ src/main/java/com/mediafire/sdk/tokenfarm/MFTokenDistributor.java
@@ -0,0 +1,20 @@
+package com.mediafire.sdk.tokenfarm;
+
+/**
+ * Created by Chris Najar on 7/16/2014.
+ */
+public interface MFTokenDistributor {
+    public void returnSessionToken(MFSessionToken sessionToken);
+
+    public void receiveNewSessionToken(MFSessionToken sessionToken);
+
+    public void receiveNewImageActionToken(MFImageActionToken uploadActionToken);
+
+    public void receiveNewUploadActionToken(MFUploadActionToken uploadActionToken);
+
+    public MFSessionToken borrowSessionToken();
+
+    public MFUploadActionToken borrowUploadActionToken();
+
+    public MFImageActionToken borrowImageActionToken();
+}
