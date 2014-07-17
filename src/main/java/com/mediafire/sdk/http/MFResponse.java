cabcab74c0d0d4d2e59c28c3da6b7326b94c2669
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 10:22:06 -0500

    MFResponse can now receive response via generic arg.

--- src/main/java/com/mediafire/sdk/http/MFResponse.java
+++ src/main/java/com/mediafire/sdk/http/MFResponse.java
@@ -1,5 +1,11 @@
 package com.mediafire.sdk.http;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mediafire.sdk.api_responses.ApiResponse;
+
 import java.util.List;
 import java.util.Map;
 
@@ -27,11 +33,34 @@ public final class MFResponse {
         return headers;
     }
 
-    public byte[] getBodyBytes() {
+    public byte[] getResponseAsBytes() {
         return bodyBytes;
     }
 
-    public String getResponseString() {
+    public String getResponseAsString() {
         return new String(bodyBytes);
+    }
+
+    public <ResponseClass extends ApiResponse> ResponseClass getResponseObject(Class<ResponseClass> responseClass) {
+        String responseString = getResponseAsString();
+        if (responseString == null) {
+            return null;
+        }
+        return new Gson().fromJson(getResponseStringForGson(responseString), responseClass);
+    }
+
+    public String getResponseStringForGson(String response) {
+        if (response == null || response.isEmpty()) {
+            return null;
+        }
+
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(response);
+        if (element.isJsonObject()) {
+            JsonObject jsonResponse = element.getAsJsonObject().get("response").getAsJsonObject();
+            return jsonResponse.toString();
+        } else {
+            return null;
+        }
     }
 }
