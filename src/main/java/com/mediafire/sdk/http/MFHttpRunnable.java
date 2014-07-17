b3bb09e1846da3b28d4ca6797664ab4c3e829f09
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 07:20:23 -0500

    MFHttpRunnable created.

--- src/main/java/com/mediafire/sdk/http/MFHttpRunnable.java
+++ src/main/java/com/mediafire/sdk/http/MFHttpRunnable.java
@@ -1,11 +1,37 @@
 package com.mediafire.sdk.http;
 
+import com.mediafire.sdk.config.MFConfiguration;
+import com.mediafire.sdk.token.MFTokenDistributor;
+
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by Chris Najar on 7/17/2014.
  */
 public class MFHttpRunnable implements Runnable {
+    private MFTokenDistributor mfTokenDistributor;
+    private MFConfiguration mfConfiguration;
+    private MFRequest mfRequest;
+
+    public MFHttpRunnable(MFTokenDistributor mfTokenDistributor, MFConfiguration mfConfiguration, MFRequest mfRequest) {
+        this.mfTokenDistributor = mfTokenDistributor;
+        this.mfConfiguration = mfConfiguration;
+        this.mfRequest = mfRequest;
+    }
+
     @Override
     public void run() {
+        MFHttpSetup mfHttpSetup = new MFHttpSetup(mfTokenDistributor, mfConfiguration);
+        MFHttpClient mfHttpClient = new MFHttpClient(mfConfiguration);
+        MFHttpCleanup mfHttpCleanup = new MFHttpCleanup(mfTokenDistributor, mfConfiguration);
 
+        try {
+            mfHttpSetup.prepareMFRequestForHttpClient(mfRequest);
+            mfHttpClient.sendRequest(mfRequest);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        mfHttpCleanup.returnToken(mfRequest);
     }
 }
