b06cc5dbcb22fdf134158b45774646af00e51996
Christopher Najar <chris.najar@mediafire.com>
2014-07-17 10:24:49 -0500

    MFPausableExecutorService extends Executor now instead of ExecutorService.

--- src/main/java/com/mediafire/sdk/config/MFPausableExecutorService.java
+++ src/main/java/com/mediafire/sdk/config/MFPausableExecutorService.java
@@ -1,11 +1,11 @@
 package com.mediafire.sdk.config;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
 
 /**
  * Created by Chris Najar on 7/17/2014.
  */
-public interface MFPausableExecutorService extends ExecutorService {
+public interface MFPausableExecutorService extends Executor {
     public void pause();
 
     public void resume();
