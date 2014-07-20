ea9f4a432256394dc5d16bad1d4a61ff7588d10d
Christopher Najar <chris.najar@mediafire.com>
2014-07-20 17:32:50 -0500

    MFHttpRunner renamed to MFDefaultHttpProcessor and MFHttpProcessor interface added (for later refactor)

--- src/main/java/com/mediafire/sdk/config/MFDefaultHttpProcessor.java
+++ src/main/java/com/mediafire/sdk/config/MFDefaultHttpProcessor.java
@@ -1,22 +1,23 @@
-package com.mediafire.sdk.http;
+package com.mediafire.sdk.config;
 
-import com.mediafire.sdk.config.MFConfiguration;
+import com.mediafire.sdk.http.*;
 import com.mediafire.sdk.token.MFTokenFarmCallback;
 
 import java.io.UnsupportedEncodingException;
 
-public final class MFHttpRunner {
-    private static final String TAG = MFHttpRunner.class.getCanonicalName();
+public final class MFDefaultHttpProcessor implements MFHttpProcessor {
+    private static final String TAG = MFDefaultHttpProcessor.class.getCanonicalName();
     private final MFHttpClientSetup mfHttpClientSetup;
     private final MFHttpClient mfHttpClient;
     private final MFHttpClientCleanup mfHttpClientCleanup;
 
-    public MFHttpRunner(MFConfiguration mfConfiguration, MFTokenFarmCallback mfTokenFarmCallback) {
+    public MFDefaultHttpProcessor(MFConfiguration mfConfiguration, MFTokenFarmCallback mfTokenFarmCallback) {
         this.mfHttpClientSetup = new MFHttpClientSetup(mfTokenFarmCallback, mfConfiguration);
         this.mfHttpClient = new MFHttpClient(mfConfiguration);
         this.mfHttpClientCleanup = new MFHttpClientCleanup(mfTokenFarmCallback, mfConfiguration);
     }
 
+    @Override
     public MFResponse doRequest(final MFRequester mfRequester) {
         MFConfiguration.getStaticMFLogger().v(TAG, "doRequest()");
 
